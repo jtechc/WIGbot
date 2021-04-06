2d05722e4ce641261d6e079d22ba7e22aa03c0cf
techcj <zh.techc@gmail.com>
2021-04-06 19:46:52 -0400

    secure kick command

--- commands/kick.js
+++ commands/kick.js
@@ -2,13 +2,17 @@ module.exports = {
   name: 'kick',
   description: 'This command kicks a member of the discord server',
   execute(client, message, args) {
-    let member = message.mentions.users.find();
-    if (member) {
-      let memberTarget = message.guild.members.cache.get(member.id);
-      memberTarget.kick();
-      message.channel.send('User has been kicked.');
+    if (message.author.id === process.env.BOTOWNERID) {
+      let member = message.mentions.users.find();
+      if (member) {
+        let memberTarget = message.guild.members.cache.get(member.id);
+        memberTarget.kick();
+        message.channel.send('User has been kicked.');
+      } else {
+        message.channel.send("You couldn't kick that member.");
+      }
     } else {
-      message.channel.send("You couldn't kick that member");
+      message.channel.send("You don't have the permission to do that.");
     }
   },
 };
