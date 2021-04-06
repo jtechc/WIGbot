4f9e9f5a66910a9eacf45e77071be25a01f012c7
techcj <zh.techc@gmail.com>
2021-04-06 19:36:15 -0400

    secure ban command

--- commands/ban.js
+++ commands/ban.js
@@ -1,14 +1,18 @@
 module.exports = {
   name: 'ban',
-  description: 'This command bans a member of the discord',
+  description: 'The command bans a member of the discord',
   execute(client, message, args) {
-    let member = message.mentions.users.first();
-    if (member) {
-      let memberTarget = message.guild.members.cache.get(member.id);
-      memberTarget.ban();
-      message.channel.send('User has been banned.');
+    if (message.author.id === process.env.BOTOWNERID) {
+      let member = message.mentions.user.first();
+      if (member) {
+        let memberTarget = message.guild.members.cache.get(member.id);
+        memberTarget.ban();
+        message.channel.send('User has been banned.');
+      } else {
+        message.channel.send("You couldn't ban that member");
+      }
     } else {
-      message.channel.send("You couldn't ban that member");
+      message.channel.send("You don't have the permission to do that.");
     }
   },
 };
