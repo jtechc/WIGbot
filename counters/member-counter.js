e4a39c6bec34bdcbf116346a840a1b33c89715a7
techcj <zh.techc@gmail.com>
2021-04-09 06:14:18 -0400

    added cyan background to log

--- counters/member-counter.js
+++ counters/member-counter.js
@@ -5,6 +5,6 @@ module.exports = async (client) => {
     let memberCount = guild.memberCount;
     let channel = guild.channels.cache.get('829192388361584650');
     channel.setName(`Total Members: ${memberCount.toLocaleString()}`);
-    console.log(chalk.blue('Updating Member Count'));
+    console.log(chalk.blue.bgCyan('Updating Member Count'));
   }, 60000);
 };
