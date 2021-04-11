e5948e43212f4b67f8efbb68e3f5e38af11a1aba
Zachary Hahn <zh.techc@gmail.com>
2021-04-11 06:40:03 -0400

    slightly different membercounter

--- counters/member-counter.js
+++ counters/member-counter.js
@@ -1,14 +1,28 @@
 const chalk = require('chalk');
 module.exports = async (client) => {
-  let guild = client.guilds.cache.get('828579107830104096');
+  const guild = client.guilds.cache.get('755142481317855293');
+  if(!guild.available) return console.log('No guild found');
   setInterval(() => {
-    let memberCount = guild.memberCount;
-    let channel = guild.channels.cache.get('828614680439291984');
+    const memberCount = guild.memberCount;
+    const channel = guild.channels.cache.get('829192388361584650');
     channel.setName(`Total Members: ${memberCount.toLocaleString()}`);
     console.log(
       chalk.black.bgCyan(
-        `Checking if we need to update member count ${memberCount}`,
-      ),
-    );
-  }, 60000);
+        `Current member count: ${memberCount} | Checking if we need to update`,
+        )
+      )
+  }, 60000)
 };
+
+
+// let guild = client.guilds.cache.get('828579107830104096');
+// setInterval(() => {
+//   let memberCount = guild.memberCount;
+//   let channel = guild.channels.cache.get('828614680439291984');
+//   channel.setName(`Total Members: ${memberCount.toLocaleString()}`);
+//   console.log(
+//     chalk.black.bgCyan(
+//       `Checking if we need to update member count ${memberCount}`,
+//     ),
+//   );
+// }, 60000);
