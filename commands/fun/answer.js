2648611f363defa8ef9ba986aaad12a6b14d72e2
Zachary Hahn <zh.techc@gmail.com>
2021-04-16 05:35:44 -0400

    ts

--- commands/fun/answer.js
+++ commands/fun/answer.js
@@ -1,8 +1,17 @@
-module.exports = {
-    commands: ['myanswer'],
-    category: 'Fun',
-    ownerOnly: true,
-    description: 'My answer to your question',
-    callback: ({ message, })
+const { MessageEmbed } = require('discord.js')
 
+
+module.exports = {
+    commands: ['vhserver'],
+    category: 'Fun',
+    permissions: 'ADMINISTRATOR',
+    description: 'Send embed for valheim server',
+    callback: ({ message, args, text, client, channel }) => {
+        const vhChannel = channel.channels.cache.find(c => c.name == '〔🏹〕valheim-info')
+        console.log(vhChannel)
+
+        const embed = new MessageEmbed()
+
+    }
+    
 }
