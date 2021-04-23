f5d5ce24f53b2227d2c31a538fd6ed99ad1ac056
Exxon <tomfagan03@gmail.com>
2021-04-23 13:34:31 +0100

    Util commands

--- commands/util/suggestions.js
+++ commands/util/suggestions.js
@@ -2,28 +2,33 @@ const { MessageEmbed} = require('discord.js')
 const { Command } = require("cdcommands");
 
 module.exports = new Command ({
-  name: 'suggestions',
-  category: 'Info',
-  guildOnly: true,
-  aliases: ['suggest', 'suggestion'],
-  userPermissions: [],
-  description: 'creates a suggestion!',
-  run: ({ message, args, client, prefix, language }) => {
-      const channel = message.client.channels.cache.find(c => c.name === '〔💡〕suggestions');
-      if (!channel) return message.channel.send('I cannot seem to find the suggestions channel!');
+    name: 'suggestions',
+    aliases: ['suggest', 'suggestion'],
+    description: 'creates a suggestion!',
+    details: 'creates a suggestion!',
+    minArgs: 1,
+    maxArgs: Infinity,
+    usage: '{prefix}suggestions <Suggestion>',
+    guildOnly: true,
+    noDisable: false,
+    category: 'Info',
+    run: ({ message, args }) => {
+        const channel = message.client.channels.cache.find(c => c.name === '〔💡〕suggestions');
+        if (!channel)
+            return message.channel.send('I cannot seem to find the suggestions channel!');
 
-      let messageArgs = args.join(' ');
-      const embed = new MessageEmbed()
-          .setColor('FADF2E')
-          .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
-          .setDescription(messageArgs);
+        let messageArgs = args.join(' ');
+        const embed = new MessageEmbed()
+            .setColor('FADF2E')
+            .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
+            .setDescription(messageArgs);
 
-      channel.send(embed).then((msg) => {
-          msg.react('👍');
-          msg.react('👎');
-          message.delete();
-      }).catch((err) => {
-          throw err;
-      });
-  }
+        channel.send(embed).then((msg) => {
+            msg.react('👍');
+            msg.react('👎');
+            message.delete();
+        }).catch((err) => {
+            console.log(err)
+        });
+    }
 })
